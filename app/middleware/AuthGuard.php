9bcf1c1aae3e65910599eb6f0e1716db468bab4f
JuanEstebanAparicio <apariciojuanesteban@gmail.com>
2025-10-20 18:52:22 -0500

    creacion de guardias

--- app/middleware/AuthGuard.php
+++ app/middleware/AuthGuard.php
@@ -1,23 +1,8 @@
 <?php
-// PROJECTDELIX/app/middleware/AuthGuard.php
-
+// 📂 gestor_empleado/guards/authGuard.php
 session_start();
-
-// Si no existe una sesión de admin activa, redirigir al login
-if (!isset($_SESSION['admin'])) {
-    header('Location: ../public/index.php');
-
+if (!isset($_SESSION['user_id']) || !isset($_SESSION['rol'])) {
+    header("Location: /login.php");
     exit;
 }
-
-// Opcional: proteger contra inactividad (expiración de sesión)
-$tiempoMaximo = 60 * 60; // 1 hora
-if (isset($_SESSION['ultimo_acceso']) && (time() - $_SESSION['ultimo_acceso'] > $tiempoMaximo)) {
-    session_unset();
-    session_destroy();
-    header('Location: ../public/index.php?expired=true');
-    exit;
-}
-
-// Actualiza el tiempo del último acceso
-$_SESSION['ultimo_acceso'] = time();
+?>
