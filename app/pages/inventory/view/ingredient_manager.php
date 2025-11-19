f361784e3da42b80d36a6e6a73c813222d1275cc
JuanEstebanAparicio <apariciojuanesteban@gmail.com>
2025-11-19 14:53:23 -0500

    adaptacion de ingredient_manager

--- app/pages/inventory/view/ingredient_manager.php
+++ app/pages/inventory/view/ingredient_manager.php
@@ -22,8 +22,8 @@ require_once __DIR__ . '/../../../config/supabase.php';
 if ($usuario['tipo'] === 'propietario') {
     $id_usuario = $usuario['id'];
 } elseif ($usuario['tipo'] === 'empleado') {
-    $id_usuario = $usuario['restaurant_id'];
-} else {
+    $id_usuario = $usuario['user_id'];
+}else {
     header("Location: /DelixSystem/public/index.php");
     exit;
 }
